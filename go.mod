0d3ff4a4dd0f672e64ee5458ae0d2d96de6555a6
Loric Brevet <loric.brevet@gmail.com>
2019-10-01 22:39:23 +0200

    Structure a little more the implementation

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/PuerkitoBio/goquery v1.5.0
+	github.com/andybalholm/cascadia v1.0.0
 	github.com/davecgh/go-spew v1.1.1
 	github.com/guptarohit/asciigraph v0.4.1
 	golang.org/x/net v0.0.0-20190923162816-aa69164e4478
--- main.go
+++ main.go
@@ -6,10 +6,15 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/guptarohit/asciigraph"
+	"golang.org/x/net/html"
 )
+
+var IgnoreTags = []string{"script", "link", "style", "meta"}
+var StructuralTags = []string{"p", "table", "br", "div", "li", "h1", "h2", "h3", "h4", "h5", "h6"}
 
 type Article struct {
 	Img *Image
@@ -65,10 +70,17 @@ func From(r io.Reader) (*Article, error) {
 		return nil, err
 	}
 
+	// TODO(lobre) strip useless bits of the document
+	if err := a.strip(); err != nil {
+		return nil, err
+	}
+
 	// TODO(lobre) extract document in some sort of data structure
 	if err := a.extract(); err != nil {
 		return nil, err
 	}
+
+	// TODO assert if really an article
 
 	return &a, nil
 }
@@ -81,14 +93,24 @@ func (a *Article) metadata() error {
 	return nil
 }
 
-func (a *Article) extract() error {
+func (a *Article) strip() error {
 	// init clip from body
 	a.clip = a.Doc.Find("body")
 
-	// remove unwanted tags "link", "script", "style", "meta"
+	// remove unwanted tags
+	a.clip.Find(strings.Join(IgnoreTags, ",")).Remove()
+
+	// remove classes and id
+	a.clip.Find("*").RemoveClass().RemoveAttr("id")
+
+	// remove blank lines
+
+	return nil
+}
+
+func (a *Article) extract() error {
 	// analyse structural tags "p", "table", "br", "div", "h1", "h2", "h3", "h4", "h5", "h6", "li"
 	// parse into a data structure that will easily allow outputs
-
 	return nil
 }
 
@@ -103,12 +125,31 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-	art, err := From(resp.Body)
+	inline := `<html><body><div class="outter-class">
+        <h1 class="inner-class">
+	        The string I need
+
+	        <span class="other-class" >Some value I don't need</span>
+	        <span class="other-class2" title="sometitle"></span>
+        </h1>
+
+        <div class="other-class3">
+            <h3>Some heading i don't need</h3>
+        </div>
+    </div></body></html>`
+
+	art, err := From(strings.NewReader(inline))
+	//art, err := From(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(art.Text())
+	html, err := art.Html()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(html)
 	fmt.Println(art.Plot())
 }
 
@@ -127,4 +168,25 @@ func (a *Article) Markdown() (string, error) {
 func (a *Article) Plot() string {
 	data := []float64{3, 4, 9, 6, 2, 4, 5, 8, 5, 10, 2, 7, 2, 5, 6}
 	return asciigraph.Plot(data, asciigraph.Height(30))
+}
+
+func iterate(doc *html.Node, do func(n *html.Node) error) error {
+	var f func(n *html.Node) error
+	f = func(n *html.Node) error {
+		if err := do(n); err != nil {
+			return err
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			if err := f(c); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	if err := f(doc); err != nil {
+		return err
+	}
+
+	return nil
 }
